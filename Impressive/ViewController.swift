788c4151397bc96e15b9eb5bc6388fadc070fac5
Andrew Schmidt <Andrew@Andrews-MacBook.local>
2015-02-17 15:37:52 -0800

    Initial Commit

---
+++ Impressive/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Impressive
+//
+//  Created by Andrew Schmidt on 2/17/15.
+//  Copyright (c) 2015 Andrew Schmidt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
